80a6b048692f7d6fe23f08a2fbe1e906e5eaf5f9
Daniel Tomaszuk <daniel.tomaszuk@netguru.pl>
2020-04-18 11:41:37 +0200

    feature. Add init project structure.

---
+++ Makefile
@@ -0,0 +1,17 @@
+update-deps:
+	pip install pip-tools
+	pip-compile --generate-hashes ./app/requirements/dev.in --output-file ./app/requirements/dev.txt
+	pip-compile --generate-hashes ./app/requirements/requirements.in --output-file ./app/requirements/requirements.txt
+
+format:
+	black ./app --skip-string-normalization
+	isort --apply
+
+run-tests:
+	pytest -v ./app/
+
+build:
+	@docker-compose up --force-recreate --build
+
+up:
+	@docker-compose up
---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+import os
+from typing import List
+
+from starlette.config import Config
+from starlette.datastructures import CommaSeparatedStrings, Secret
+
+BASE_DIR = os.path.dirname(os.path.realpath(__file__)).rsplit(os.sep, 1)[0]
+# statics config
+STATIC_URL = "/static"
+STATIC_ROOT = BASE_DIR + "/static"
+
+config = Config(".env")
+
+PROJECT_NAME = config("webrtc-hub", default="webrtc-hub")
+
+SECRET_KEY: Secret = config("SECRET_KEY", cast=Secret)
+DEBUG = config("DEBUG", cast=bool, default=False)
+
+ALLOWED_HOSTS: List[str] = config("ALLOWED_HOSTS", cast=CommaSeparatedStrings, default="")
+
+# Azure Tracer
+# connection string taken from Azure App Insights
+APPLICATIONINSIGHTS_CONNECTION_STRING = config("APPLICATIONINSIGHTS_CONNECTION_STRING", default="")
