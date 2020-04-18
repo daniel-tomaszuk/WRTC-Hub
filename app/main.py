17b95dd3d0e9857987305b68145df5f89495da48
dghy <dtomaszuk01@gmail.com>
2020-04-18 15:57:00 +0200

    test. Circe test.

--- app/main.py
+++ app/main.py
@@ -31,4 +31,4 @@ app = get_application()
 
 if __name__ == "__main__":
     # entrypoint for starting the app as python script - `python main.py` will start the worker
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)  # test
